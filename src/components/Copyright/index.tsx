4e82ef4888e333990d6d95793cdf0400d96739b3
sepiiroth <37743946+sepiiroth@users.noreply.github.com>
2025-03-10 12:31:41 +0100

    Reset Portfolio

--- src/components/Copyright/index.tsx
+++
@@ -1,9 +0,0 @@
-export function Copyright() {
-
-    return (
-        <footer className="container mx-auto px-4 text-center absolute bottom-0 w-100">
-            <p className="text-xs">
-                Copyright © 2025 NUNES Joao-Rafael. Tous droits réservés.
-            </p>
-        </footer>);
-}
